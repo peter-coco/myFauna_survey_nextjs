d4fccd87679bb076a7d1401183fc9592a7fc9080
peter-coco <dkliser@gmail.com>
2022-09-11 17:27:10 +0900

    [Feat] UI-main page

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,9 +1,23 @@
+import { cls } from '@utils/function/cls';
+
 interface Props {
   text: string;
+  type: 'homeNintro' | 'surveyNResult';
+  onClick?: () => void;
 }
 
-const Button = ({ text }: Props) => {
-  return <button>{text}</button>;
+const Button = ({ text, type, onClick }: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cls(
+        'rounded-full shadow-2xl w-1/2 h-14 font-semibold text-gray-900 border-2 border-gray-300',
+        type === 'surveyNResult' ? 'bg-white' : 'bg-red-300'
+      )}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,26 @@ import Button from '@components/button/Button';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push('/intro');
+  };
   return (
-    <div>
+    <div className="px-4 py-10 max-w-md min-w-fit mx-auto flex flex-col items-center justify-between h-screen">
       <Head>
         <title>나는 어떤 타입의 동물일까?</title>
       </Head>
-      <h1>나는 어떤 타입의 동물일까?</h1>
-      <img src=""></img>
-      <Button text="테스트 시작!" />
-      <h3>참여자 수 : 1000000</h3>
+      <h1 className="font-bold text-xl text-gray-900">나는 어떤 타입의 동물일까?</h1>
+      <Image width={246} height={280} src="/images/main.jpeg" alt="main-image" />
+      <Button text="테스트 시작!" type="homeNintro" onClick={onClick} />
+      <h3 className="font-bold text-lg text-center text-gray-900">
+        참여자 수 <div> 1000000</div>
+      </h3>
     </div>
   );
 };
